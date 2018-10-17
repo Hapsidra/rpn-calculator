6063c93c9f2920495845fb6e89265f80b7748330
Max Efimov <hapsidra@gmail.com>
2018-10-17 18:32:31 +0900

    refactoring

--- src/Calculator.java
+++ src/Calculator.java
@@ -37,109 +37,25 @@ public class Calculator implements ActionListener {
         JPanel buttonsPanel = new JPanel();
         buttonsPanel.setPreferredSize(new Dimension(program.getWidth() - 10,program.getHeight() - 80));
 
-        buttonsPanel.setLayout(new GridLayout(5,4,5,5));
+        String[][]buttons = new String[][] {
+                {"C", "(", ")", "/"},
+                {"7", "8", "9", "*"},
+                {"4", "5", "6", "-"},
+                {"1", "2", "3", "+"},
+                {"+-","0", "<", "="}
+        };
+
+        buttonsPanel.setLayout(new GridLayout(buttons.length, buttons[0].length,5,5));
         program.add(buttonsPanel);
 
-        Button buttonCE = new Button("C");
-        buttonCE.addActionListener(this);
-        buttonCE.setFocusable(false);
-        buttonsPanel.add(buttonCE);
-
-        Button buttonC = new Button("(");
-        buttonC.addActionListener(this);
-        buttonC.setFocusable(false);
-        buttonsPanel.add(buttonC);
-
-        Button buttonDel = new Button(")");
-        buttonDel.addActionListener(this);
-        buttonDel.setFocusable(false);
-        buttonsPanel.add(buttonDel);
-
-        Button buttonDiv = new Button("/");
-        buttonDiv.addActionListener(this);
-        buttonDiv.setFocusable(false);
-        buttonsPanel.add(buttonDiv);
-
-        Button button7 = new Button("7");
-        button7.addActionListener(this);
-        button7.setFocusable(false);
-        buttonsPanel.add(button7);
-
-        Button button8 = new Button("8");
-        button8.addActionListener(this);
-        button8.setFocusable(false);
-        buttonsPanel.add(button8);
-
-        Button button9 = new Button("9");
-        button9.addActionListener(this);
-        button9.setFocusable(false);
-        buttonsPanel.add(button9);
-
-        Button buttonMul = new Button("*");
-        buttonMul.addActionListener(this);
-        buttonMul.setFocusable(false);
-        buttonsPanel.add(buttonMul);
-
-        Button button4 = new Button("4");
-        button4.addActionListener(this);
-        button4.setFocusable(false);
-        buttonsPanel.add(button4);
-
-        Button button5 = new Button("5");
-        button5.addActionListener(this);
-        button5.setFocusable(false);
-        buttonsPanel.add(button5);
-
-        Button button6 = new Button("6");
-        button6.addActionListener(this);
-        button6.setFocusable(false);
-        buttonsPanel.add(button6);
-
-        Button buttonSub = new Button("-");
-        buttonSub.addActionListener(this);
-        buttonSub.setFocusable(false);
-        buttonsPanel.add(buttonSub);
-
-        Button button1 = new Button("1");
-        button1.addActionListener(this);
-        button1.setFocusable(false);
-        buttonsPanel.add(button1);
-
-        Button button2 = new Button("2");
-        button2.addActionListener(this);
-        button2.setFocusable(false);
-        buttonsPanel.add(button2);
-
-        Button button3 = new Button("3");
-        button3.addActionListener(this);
-        button3.setFocusable(false);
-        buttonsPanel.add(button3);
-
-        Button buttonAdd = new Button("+");
-        buttonAdd.addActionListener(this);
-        buttonAdd.setFocusable(false);
-        buttonsPanel.add(buttonAdd);
-
-        Button buttonPlusMinus = new Button("+-");
-        buttonPlusMinus.addActionListener(this);
-        buttonPlusMinus.setFocusable(false);
-        buttonsPanel.add(buttonPlusMinus);
-
-        Button button0 = new Button("0");
-        button0.addActionListener(this);
-        button0.setFocusable(false);
-        buttonsPanel.add(button0);
-
-        Button buttonDot = new Button("<");
-        buttonDot.addActionListener(this);
-        buttonDot.setFocusable(false);
-        buttonsPanel.add(buttonDot);
-
-        Button buttonResult = new Button("=");
-        buttonResult.addActionListener(this);
-        buttonResult.setFocusable(false);
-        buttonsPanel.add(buttonResult);
-
+        for (int i = 0; i < buttons.length; i++) {
+            for (int j = 0; j < buttons[i].length; j++) {
+                Button button = new Button(buttons[i][j]);
+                button.addActionListener(this);
+                button.setFocusable(false);
+                buttonsPanel.add(button);
+            }
+        }
         buttonsPanel.setVisible(true);
         program.setVisible(true);
     }
